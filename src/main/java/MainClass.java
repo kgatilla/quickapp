56ef9867d422cf6ef4035c58479eaad4745bd530
kgatilla <kissgazda.atilla@gmail.com>
2018-03-20 17:14:48 +0100

    Main app entry point

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,16 @@
+import datastore.H2DBUtilities;
+
+public class MainClass {
+
+
+    private static void spinUpH2DB() {
+        H2DBUtilities util = new H2DBUtilities();
+        util.createNewDbFromScratch();
+    }
+
+    public static void main(String... args) {
+        spinUpH2DB();
+
+        //while(true){}
+    }
+}
