0e9b8658565dbf48983ed510db8b0341423bca96
kgatilla <kissgazda.atilla@gmail.com>
2018-04-09 09:17:18 +0200

    remove interfaces from model classes

--- src/main/java/payments/model/BankAccount.java
+++ src/main/java/payments/model/BankAccount.java
@@ -2,19 +2,81 @@ package payments.model;
 
 import org.joda.money.CurrencyUnit;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
-public interface BankAccount {
+public final class BankAccount {
 
-    int getAccountId();
+    public static BankAccount of(int accountId, String bic, String iban, String ukSortCode, String ukAccountNumber, CurrencyUnit currency, final Set<BankAccountHolder> owners) {
+        return new BankAccount(accountId,bic,iban,ukSortCode,ukAccountNumber,currency, owners);
+    }
 
-    String getBIC();
-    String getIBAN();
-    String getUKSortCode();
-    String getUKAccountNumber();
 
-    CurrencyUnit getCurrency();
+    public static BankAccount of(int accountId, String bic, String iban, String ukSortCode, String ukAccountNumber, CurrencyUnit currency, BankAccountHolder owner) {
+        HashSet<BankAccountHolder> owners = new HashSet<>();
+        owners.add(owner);
+        return new BankAccount(accountId,bic,iban,ukSortCode,ukAccountNumber,currency, owners);
+    }
 
-    // for joint accounts
-    Set<BankAccountHolder> getOwners();
+    private BankAccount(int accountId, String bic, String iban, String ukSortCode, String ukAccountNumber, CurrencyUnit currency, final Set<BankAccountHolder> owners) {
+        this.accountId = accountId;
+        this.BIC = bic;
+        this.IBAN = iban;
+        this.UKSortCode = ukSortCode;
+        this.UKAccountNumber = ukAccountNumber;
+        this.currency = currency;
+        this.owners = new HashSet<>();
+        this.owners.addAll(owners);
+    }
+
+    public int getAccountId() {
+        return this.accountId;
+    }
+
+    public String getBIC() {
+        return this.BIC;
+    }
+
+    public String getIBAN() {
+        return this.IBAN;
+    }
+
+    public String getUKSortCode() {
+        return this.UKSortCode;
+    }
+
+    public String getUKAccountNumber() {
+        return this.UKAccountNumber;
+    }
+
+    public CurrencyUnit getCurrency() {
+        return this.currency;
+    }
+
+    public Set<BankAccountHolder> getOwners() {
+        //TODO: consider defensive copy
+        return Collections.unmodifiableSet(this.owners);
+    }
+
+    private final int accountId;
+    private final String BIC;
+    private final String IBAN;
+    private final String UKSortCode;
+    private final String UKAccountNumber;
+    private final CurrencyUnit currency;
+    private final Set<BankAccountHolder> owners;
+
+    @Override
+    public boolean equals(Object other){
+        if (this == other) return true;
+        if (null == other) return false;
+        if (! (other instanceof BankAccount)) return false;
+        return this.accountId == ((BankAccount) other).getAccountId();
+    }
+
+    @Override
+    public int hashCode(){
+        return accountId;
+    }
 }
--- src/main/java/payments/model/BankAccountHolder.java
+++ src/main/java/payments/model/BankAccountHolder.java
@@ -1,41 +1,36 @@
 package payments.model;
 
-public final class BankClient implements BankAccountHolder, Comparable<BankClient> {
+public final class BankAccountHolder {
 
-    public BankClient( int id, String firstName, String lastName, String email){
+    public BankAccountHolder(int id, String firstName, String lastName, String email){
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-    @Override
     public int getClientId() {
         return id;
     }
 
-    @Override
     public String getFirstName() {
         return firstName;
     }
 
-    @Override
     public String getLastName() {
         return lastName;
     }
 
-    @Override
     public String getEmail() {
         return email;
     }
 
-    @Override
     public boolean equals(Object other){
         if (this == other) return true;
         if (other == null) return false;
-        if (! (other instanceof BankClient)) return false;
-        if (this.id != ((BankClient) other).getClientId()) return false;
-        return this.email.compareToIgnoreCase(((BankClient) other).getEmail()) == 0;
+        if (! (other instanceof BankAccountHolder)) return false;
+        if (this.id != ((BankAccountHolder) other).getClientId()) return false;
+        return this.email.compareToIgnoreCase(((BankAccountHolder) other).getEmail()) == 0;
     }
 
     @Override
@@ -47,9 +42,4 @@ public final class BankClient implements BankAccountHolder, Comparable<BankClien
     private final String firstName;
     private final String lastName;
     private final String email;
-
-    @Override
-    public int compareTo(BankClient that) {
-        return this.getClientId() - that.getClientId();
-    }
 }
